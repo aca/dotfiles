87ee33eddde8940b79ec52c580b4faf22498a1c1
aca <acadx0@gmail.com>
2021-01-08 07:52:56 +0900

    .

---
+++ surfingkeys/config.js
@@ -0,0 +1,40 @@
+// mappings
+// https://github.com/brookhong/Surfingkeys/blob/master/pages/default.js
+
+settings.smoothScroll = false;
+settings.scrollStepSize = 140;
+// settings.blacklistPattern = /localhost|127.0.0.1|.*docs.google.com.*|.*resumedraft.action.*/i;
+settings.blacklistPattern = /.*codesandbox.*|.*docs.google.com.*|.*resumedraft.action.*/i;
+
+unmap("'");
+unmap("w");
+unmap("b");
+unmap("r");
+unmap("x");
+unmap("X");
+unmap(";");
+unmap("<Alt-b>");
+unmap("<Alt-d>");
+unmap("<Alt-f>");
+unmap("<Alt-w>");
+unmap("<Ctrl-,>");
+unmap("<Ctrl-.>");
+unmap("<Ctrl-6>");
+unmap("<Ctrl-D>");
+unmap("<Ctrl-'>");
+unmap("<Ctrl-c>");
+unmap("<Ctrl-d>");
+unmap("<Ctrl-e>");
+unmap("<Ctrl-i>");
+unmap("<Ctrl-m>");
+unmap("<Ctrl-n>");
+unmap("<Ctrl-p>");
+unmap("<Ctrl-u>");
+unmap("I");
+unmap("i");
+unmap("m");
+
+// map('<Meta-l>', 't'); // override address bar (win/mac)
+map('bf', 'gf');
+map('<Ctrl-[>', 'S');
+map('<Ctrl-]>', 'D');
