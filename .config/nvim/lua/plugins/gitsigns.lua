a75085c42f1fb553b5567e99ffe21be20828da4c
aca <acadx0@gmail.com>
2021-09-05 17:49:29 +0900

    .

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,7 @@
 -- https://github.com/lewis6991/gitsigns.nvim
-vim.cmd("packadd gitsigns.nvim")
+vim.cmd [[
+"packadd gitsigns.nvim"
+]]
 
 require("gitsigns").setup {
     signs = {
@@ -11,22 +13,24 @@ require("gitsigns").setup {
     },
     numhl = false,
     linehl = false,
-    signcolumn = false,
+    signcolumn = true,
     keymaps = {
         -- Default keymap options
-        noremap = true,
+        noremap = false,
         buffer = true,
         ["n ]h"] = {expr = true, '&diff ? \']c\' : \'<cmd>lua require"gitsigns.actions".next_hunk()<CR>\''},
         ["n [h"] = {expr = true, '&diff ? \'[c\' : \'<cmd>lua require"gitsigns.actions".prev_hunk()<CR>\''},
-        ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-        ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-        ["n <leader>hR"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-        ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-        -- Text objects
-        ["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-        ["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
+
+        ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+        ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+        ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+        ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+        ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+        ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+        ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
+        ['n <leader>hU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
     },
     watch_index = {
         interval = 1000
