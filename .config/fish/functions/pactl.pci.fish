04f43c6a349bba0475f53c4ce44faeb48c5d885d
Kyungrok Chung <acadx0@gmail.com>
2021-12-11 10:13:04 +0900

    .

--- .config/fish/functions/pactl.pci.fish
+++ .config/fish/functions/pactl.pci.fish
@@ -1,6 +1,6 @@
 function pactl.pci
   command -q pactl || return 1
-  set SINK_NEXT (pactl list sinks short | grep 'hw_sofhdadsp__sink' | awk '{print $1}')
+  set SINK_NEXT (pactl list sinks short | grep 'alsa_output.pci-0000_01_00.1.hdmi-stereo' | awk '{print $1}')
   for i in (pactl list sink-inputs short | awk '{print $1}')
     pactl move-sink-input $i $SINK_NEXT
   end
