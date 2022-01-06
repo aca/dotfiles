06d01164c15db236751ef36617878067a71cadae
rok <acadx0@gmail.com>
2022-01-06 22:34:02 +0900

    .

--- .config/nvim/autoload/command/makelink.lua
+++ .config/nvim/autoload/command/makelink.lua
@@ -9,16 +9,15 @@ local function replace(str, what, with)
 	return v
 end
 
-
 vim.api.nvim_add_user_command("Makelink", function()
 	local line = api.nvim_get_current_line()
 	local url = string.match(line, "[http://][https://][%w|%p]*")
 
 	-- http://www.pixelbeat.org/cmdline.html
 	-- sed -n 's/.*<title>\(.*\)<\/title>.*/\1/ip;T;q'
-  -- TODO: fix not working on mac, need to test on linux
+	-- TODO: fix not working on mac, need to test on linux
 	local cmd = 'curl -s --fail "' .. url .. '"' .. "|" .. "sed -n 's/.*<title>\\(.*\\)<\\/title>.*/\\1/ip;T;q'"
-  -- print(cmd)
+	-- print(cmd)
 	local err = vim.api.nvim_get_vvar("shell_error")
 	if 0 ~= err then
 		print("failed to update link")
