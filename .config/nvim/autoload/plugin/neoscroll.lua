ebbabdd31b84bc9b2ea5681f7a3f096e05803892
Kyungrok Chung <acadx0@gmail.com>
2022-01-10 12:41:12 +0900

    .

--- .config/nvim/autoload/plugin/neoscroll.lua
+++ .config/nvim/autoload/plugin/neoscroll.lua
@@ -3,6 +3,6 @@ vim.cmd([[
 ]])
 
 require("neoscroll").setup({
-    -- mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
-    mappings = {'<C-u>', '<C-d>'},
+	-- mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+	mappings = { "<C-u>", "<C-d>" },
 })
